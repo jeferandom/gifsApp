e4cd75c12ef770912ffa44afaeea698157a939c4
Jeferandom <jheffmancera@gmail.com>
2022-03-22 17:22:44 -0500

    gifsService

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,9 +10,15 @@ export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;//@note ! not Null Assertion Operator
 
+  constructor(private gifsService: GifsService){}
+ 
   buscar() {
     const valor = this.txtBuscar.nativeElement.value;
-    console.log('valor', valor);
+    if(valor.trim().length===0){
+      return;
+    }
+
+    this.gifsService.buscarGifs(valor);    
     this.txtBuscar.nativeElement.value = '';
   }
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'//@note eleva este servicio a una disponibilidad global y evita tener que declararlo en los providers del modulo
 })
 export class GifsService {
+  private _historial: string[] = [];
 
-  constructor() { }
+  get historial() {
+    return [...this._historial];
+  }
+
+  buscarGifs(query: string) {
+    query = query.trim();
+  if(!this.historial.includes(query)){
+    this._historial.unshift(query);
+    this._historial = this._historial.splice(0,10);
+  }
+    console.log(this._historial)
+  }
+
+
 }
