136e461841e3ef06dec20e31ce01faa84ea07d02
Jeferandom <jheffmancera@gmail.com>
2022-03-23 18:52:25 -0500

    Gifs rendering

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,27 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gif.interface';
 
 @Injectable({
   providedIn: 'root'//@note eleva este servicio a una disponibilidad global y evita tener que declararlo en los providers del modulo
 })
 export class GifsService {
   private _historial: string[] = [];
+
   private apikey: string = 'es1Be5tdbAymLQFhIO6J5c2pLYT0CCzW';
+
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
   constructor(private http: HttpClient) { }
+
+
+
+
+
   buscarGifs(query: string) {
+
     query = query.trim();
+
     if (!this.historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
     }
-    console.log(this._historial)
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=es1Be5tdbAymLQFhIO6J5c2pLYT0CCzW&q=goku&limit=10')
-      .subscribe((resp: any) => {
+
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=es1Be5tdbAymLQFhIO6J5c2pLYT0CCzW&q=${query}&limit=10`)
+      .subscribe((resp) => {
         console.log(resp.data)
+        this.resultados = resp.data;
       })
   }
 
