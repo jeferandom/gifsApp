a4a11208a678635fd7cc26182fb81ae1183ace69
Jeferandom <jheffmancera@gmail.com>
2022-03-23 15:22:34 -0500

    Consuming http api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,18 +6,23 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _historial: string[] = [];
+  private apikey: string = 'es1Be5tdbAymLQFhIO6J5c2pLYT0CCzW';
 
   get historial() {
     return [...this._historial];
   }
-
+  constructor(private http: HttpClient) { }
   buscarGifs(query: string) {
     query = query.trim();
-  if(!this.historial.includes(query)){
-    this._historial.unshift(query);
-    this._historial = this._historial.splice(0,10);
-  }
+    if (!this.historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
     console.log(this._historial)
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=es1Be5tdbAymLQFhIO6J5c2pLYT0CCzW&q=goku&limit=10')
+      .subscribe((resp: any) => {
+        console.log(resp.data)
+      })
   }
 
 
